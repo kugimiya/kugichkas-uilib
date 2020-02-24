0f98cd26cbe38f7619b789582fa52c0df2a505ee
kugimiya <bp.001@yandex.ru>
2020-02-24 10:06:31 +0400

    Updates

---
+++ precache-manifest.1c162a91476faa2847235c6397229e66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21494de56c107ddd20ca1f6a06e5352d",
+    "url": "/react-styleguidist-demo/index.html"
+  },
+  {
+    "revision": "3569b0c0dba86d1d8434",
+    "url": "/react-styleguidist-demo/static/css/main.bc9236f6.chunk.css"
+  },
+  {
+    "revision": "f93f5a6a71b02adffdb7",
+    "url": "/react-styleguidist-demo/static/js/2.ead9738c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-styleguidist-demo/static/js/2.ead9738c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3569b0c0dba86d1d8434",
+    "url": "/react-styleguidist-demo/static/js/main.44977cdd.chunk.js"
+  },
+  {
+    "revision": "36193d953032fdc06806",
+    "url": "/react-styleguidist-demo/static/js/runtime-main.4640461c.js"
+  }
+]);
---
+++ styleguide/precache-manifest.7e5052daf9fbf14f21118b27371ce5be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a9130f80bec8eedcdbd",
+    "url": "build/2.0a9130f8.js"
+  },
+  {
+    "revision": "355dd9941dfffd671fa0ae6870cc1353",
+    "url": "build/2.0a9130f8.js.LICENSE.txt"
+  },
+  {
+    "revision": "47e562ce73a2b423b95d",
+    "url": "build/bundle.47e562ce.js"
+  },
+  {
+    "revision": "d4f75e3b0c266bc7e233",
+    "url": "build/main.d4f75e3b.js"
+  },
+  {
+    "revision": "7dedd541c3b0edfbbee1c555bb06bd7f",
+    "url": "index.html"
+  },
+  {
+    "revision": "d4f75e3b0c266bc7e233",
+    "url": "static/css/main.076d247c.chunk.css"
+  }
+]);
